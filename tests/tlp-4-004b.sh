b179a32a399f2e947e2fd2482fd921809a0a16b1
TvrtkoUrsulin <a@b>
2006-01-19 16:19:37 +0000

    New process exclusion test.
    [git-p4: depot-paths = "//dev/talpa/main/": change = 31709]

---
+++ tests/tlp-4-004b.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+#
+# TALPA test script
+#
+# Copyright (C) 2004 Sophos Plc, Oxford, England.
+#
+# This program is free software; you can redistribute it and/or modify it under the terms of the
+# GNU General Public License Version 2 as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+# even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with this program; if not,
+# write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+#
+
+insmod ../talpa_core.${ko}
+insmod ../talpa_vcdevice.${ko}
+insmod ../talpa_pedconnector.${ko}
+insmod ../talpa_${interceptor_module}.${ko}
+
+echo disable >/proc/sys/talpa/intercept-filters/DebugSyslog/status
+echo +proc >/proc/sys/talpa/intercept-filters/FilesystemExclusionProcessor/fstypes
+echo /tmp/tlp-test/ >/proc/sys/talpa/intercept-filters/FilesystemInclusionProcessor/include-path
+echo enable >/proc/sys/talpa/intercept-filters/FilesystemInclusionProcessor/status
+echo enable >/proc/sys/talpa/intercept-filters/ProcessExclusionProcessor/status
+echo enable >/proc/sys/talpa/intercept-filters/Cache/status
+echo enable >/proc/sys/talpa/intercept-filters/DegradedModeProcessor/status
+echo enable >/proc/sys/talpa/interceptors/${interceptor_name}/status
+
+./tlp-4-003a &
+
+sleep 1
+
+exit 0
