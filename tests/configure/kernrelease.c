8ddd167d9c7351701b4473439c10cf737dbe38a3
TvrtkoUrsulin <a@b>
2006-10-20 11:17:35 +0000

    [FML 1735] Configure script fixed to handle new UTS_RELEASE location.
    [git-p4: depot-paths = "//dev/talpa/main/": change = 71496]

--- tests/configure/kernrelease.c
+++ tests/configure/kernrelease.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
+#ifdef SEPARATE_UTS_RELEASE
+#include "linux/utsrelease.h"
+#else
 #include "linux/version.h"
+#endif
 
 int main()
 {
   puts(UTS_RELEASE);
-  
+
   return 0;
 }
