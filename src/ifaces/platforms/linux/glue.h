706d8cbb856f0b7a96a0a03e9148831591560514
TvrtkoUrsulin <a@b>
2007-11-28 12:12:34 +0000

    Mark 2.6.24 as supported.
    [git-p4: depot-paths = "//dev/talpa/main/": change = 131986]

--- src/ifaces/platforms/linux/glue.h
+++ src/ifaces/platforms/linux/glue.h
@@ -60,7 +60,7 @@
 /* FIXME: This is not really a fixme, but a reminder that
           the avaibility of the constant below must be checked
           in new kernels.
-          Last-known-good: 2.6.23 */
+          Last-known-good: 2.6.24 */
 #define PF_TALPA_INTERNAL    0x80000000
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(2,6,0)
