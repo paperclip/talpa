e6a7dd4bebbc9ddff3f2da7c4b626bfc5b9524ce
Douglas Leeder <douglas.leeder@sophos.com>
2012-10-30 12:39:55 +0000

    [1.14.0] Fix vfs_mount changes for older kernels.
    [git-p4: depot-paths = "//dev/talpa/main/": change = 2261263]

--- src/ifaces/platforms/linux/vfs_mount.h
+++ src/ifaces/platforms/linux/vfs_mount.h
@@ -1,6 +1,35 @@
+/*
+ * vfs_mount.h
+ *
+ * TALPA Platform code
+ *
+ * Copyright (C) 2004-2011 Sophos Limited, Oxford, England.
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License Version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if not,
+ * write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ *
+ */
 
+#ifndef H_VFS_MOUNT
+#define H_VFS_MOUNT
+
+#include <linux/kernel.h>
 #include <linux/version.h>
+#define __NO_VERSION__
+#include <linux/module.h>
 #include <linux/mount.h>
+
+#include "platforms/linux/bool.h"
+#include "platforms/linux/alloc.h"
+#include "platforms/linux/glue.h"
+#include "platforms/linux/locking.h"
 
 struct vfsmount* getParent(struct vfsmount* mnt);
 
@@ -13,3 +42,5 @@ const char *getDeviceName(struct vfsmount* mnt);
 
 
 struct dentry *getVfsMountPoint(struct vfsmount* mnt);
+
+#endif /* H_VFS_MOUNT */
