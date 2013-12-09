4e2b4b34c30bd32ccc20e6c5b7968d8ba1e97fd5
Rita Sequeira <rita.sequeira@sophos.com>
2013-12-09 13:53:16 +0000

    [3.8] Add some changes required for tests on new Ubuntu 12.04 3.8.0-25 x86-32 kernel.
    linux/compiler.h has moved - can't be located by the tests anymore for 3.8.0-35 x86-32 Ubuntu kernel.
    
    Includes some changes to support 3.13 (configure script).
    
    [+DLCL]
    
    [git-p4: depot-paths = "//dev/talpa/main/": change = 2368749]

---
+++ tests/configure/unlink_delegate.c
@@ -0,0 +1,29 @@
+#ifndef KBUILD_BASENAME
+#define KBUILD_BASENAME "#unlinktest"
+#endif
+
+#include "autoconf.h"
+
+#include <linux/version.h>
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(3,8,0)
+#include <linux/posix_types.h>
+
+#ifndef __kernel_long_t
+typedef long   __kernel_long_t;
+#endif
+
+#ifndef __kernel_ulong_t
+typedef unsigned long   __kernel_ulong_t;
+#endif
+#endif
+
+#include <linux/fs.h>
+
+int main()
+{
+    struct dentry *dentry = NULL;
+    struct inode *inode = NULL;
+
+    return vfs_unlink(inode, dentry, &inode);
+}
