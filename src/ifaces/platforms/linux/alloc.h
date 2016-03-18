75f72ade2b7816ca5bcaa96c4ebe2fb712562495
Tim Pickersgill <tim.pickersgill@sophos.com>
2016-03-18 09:18:42 +0000

    [Talpa] Update copyright for all recent changes
    [git-p4: depot-paths = "//dev/talpa/main/": change = 2582255]

--- src/ifaces/platforms/linux/alloc.h
+++ src/ifaces/platforms/linux/alloc.h
@@ -3,7 +3,7 @@
  *
  * TALPA Filesystem Interceptor
  *
- * Copyright (C) 2004-2011 Sophos Limited, Oxford, England.
+ * Copyright (C) 2004-2016 Sophos Limited, Oxford, England.
  *
  * This program is free software; you can redistribute it and/or modify it under the terms of the
  * GNU General Public License Version 2 as published by the Free Software Foundation.
